2a1713d1c0dd3bc93b36c3ac00c1382b26064984
Theinzawmaung <theinzawmaung067@gmail.com>
2019-07-19 00:10:05 +0630

    edit home page

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -2,28 +2,26 @@
 
 @section('contact')
  
-<div class="row justify-content-center">
-     <div class="col-md-8">
-           <h1 class="">All  Tickets</h1>
-          @if ($posts->isEmpty())
-            <p>There is no posts.</p>
-            @else
-            @foreach ($posts as $post)
-            <div class="card" style="margin:33px 0;">
-                <div class="card-body">
-                <h4 class="card-title">{{ $post->title }}</h4>
-                <p>{!! \Illuminate\Support\Str::limit($post->content, 100, '<a href="'. route('blog.show', $post->slug) .'">.........</a>') !!}</p>
-                <a href="{{ route('blog.show', $post->slug) }}" class="btn btn-link">Read More</a>
-                
-                </div>
-            </div>
-            @endforeach
-              
-          @endif
-         
-       </div>
-   </div>
 
+<div class="col-md-4">
+    <img src="image/1.jpg" alt="" style="margin:16px 0;" width="300px" height="300px">    
+ </div>
+ <div class="col-md-4">
+    <img src="image/2.jpg" alt="" style="margin:16px 0;" width="300px" height="300px">    
+ </div>
+ <div class="col-md-4">
+    <img src="image/3.jpg" alt="" style="margin:16px 0;" width="300px" height="300px">    
+ </div>
+
+ <div class="col-md-4">
+    <img src="image/4.jpg" alt="" style="margin:16px 0;" width="300px" height="300px">    
+ </div>
+ <div class="col-md-4">
+    <img src="image/5.jpg" alt="" style="margin:16px 0;" width="300px" height="300px">    
+ </div>
+ <div class="col-md-4">
+    <img src="image/6.jpg" alt="" style="margin:16px 0;" width="300px" height="300px">    
+ </div>
  
 @endsection
 
